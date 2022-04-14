ad3e489e0220dde11b1e9a576cb013e5b131c9b4
DenmarkPolice <albin.persson@protonmail.com>
2022-04-14 16:19:33 +0200

    started fixing linter errors

--- source/main.cpp
+++ source/main.cpp
@@ -1,28 +1,22 @@
-#include "file_editor.hpp"
+// Copyright 2022 Albin Persson
 #include <iostream>
+#include "include/file_editor.hpp"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     std::string arg_copy = "";
-    if(argc > 1)
-    {
+    if (argc > 1) {
         arg_copy = argv[1];
-    }
-    else if(argc > 2) 
-    {
+    } else if (argc > 2) {
         std::cout << "Max 2 arguments allowed\n";
         return -1;
     }
-    try
-    {
+    try {
         FileEditor editor(argc, arg_copy);
         editor.runtime();
     }
-    catch(std::invalid_argument& e)
-    {
+    catch(std::invalid_argument& e) {
         std::cerr << e.what() << std::endl;
         return -1;
     }
-    
     return 0;
 }
