580a94287d45dfc08a09e71a5a361ed0fd98e8d4
DenmarkPolice <albin.persson@protonmail.com>
2022-04-24 23:15:54 +0200

    formatPath now a part of main

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,37 @@
 #include <iostream>
 #include "include/file_editor.hpp"
 
+/*Fixes the path taken as argument when starting the program.*/
+std::string formatPath(int argc, std::string argv) {
+    char cwd[256];
+    std::string complete_path;
+    if (argc == 1 || (argc == 2 && argv == ".")) {
+        getcwd(cwd, sizeof(cwd));
+        complete_path = std::string(cwd);
+    } else {
+        char first_char_argv = argv[0];
+        if (&first_char_argv == std::string("/")) {
+            complete_path = argv;
+        } else {
+            complete_path = std::string(getcwd(cwd, sizeof(cwd))) + "/" + argv;
+        }
+    }
+    return complete_path;
+}
+
 int main(int argc, char* argv[]) {
+    std::string dir_path = "";
     if (argc > 2) {
         std::cout << "Max 2 arguments allowed\n";
         return -1;
+    } else if (argc == 2) {
+        dir_path = formatPath(argc, argv[1]);
+    } else {
+        dir_path = formatPath(argc, "");
     }
-    std::string dir_path = "";
-    if (argc > 1) {
-        dir_path = argv[1];
-    }
+    
     try {
-        FileEditor editor(argc, dir_path);
+        FileEditor editor(dir_path);
         editor.runtime();
     }
     catch(std::invalid_argument& e) {
