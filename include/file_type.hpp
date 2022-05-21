1cfb2994ed5cd0e744fc8c38ce415629358994cf
Andreas Bauer <dev@andreasbauer.org>
2022-05-22 01:41:33 +0200

    Add enum for file extension types

---
+++ include/file_type.hpp
@@ -0,0 +1,15 @@
+// Copyright 2022 Andreas Bauer
+
+#ifndef INCLUDE_FILE_TYPE_HPP_
+#define INCLUDE_FILE_TYPE_HPP_
+
+#include <iostream>
+
+namespace filetype {
+    enum class FileType { md, cpp, txt };
+
+    FileType fileTypeFromStr(const std::string val);
+    std::string fileTypeToStr(FileType type);
+}
+
+#endif
