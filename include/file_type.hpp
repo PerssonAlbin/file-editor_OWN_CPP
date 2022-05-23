7ebeba2ca4d573ecb639730b2634f24774b21b48
Andreas Bauer <andreas-bauer@users.noreply.github.com>
2022-05-23 11:59:36 +0200

    Add enum for file extension types (#38)
    * Add enum for file extension types
    
    * Fix linter issues
    
    * Add include for algorithm to solve for-each problem

---
+++ include/file_type.hpp
@@ -0,0 +1,17 @@
+// Copyright 2022 Andreas Bauer
+
+#ifndef INCLUDE_FILE_TYPE_HPP_
+#define INCLUDE_FILE_TYPE_HPP_
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+namespace filetype {
+    enum class FileType { md, cpp, txt };
+
+    FileType fileTypeFromStr(const std::string val);
+    std::string fileTypeToStr(FileType type);
+}
+
+#endif  // INCLUDE_FILE_TYPE_HPP_
