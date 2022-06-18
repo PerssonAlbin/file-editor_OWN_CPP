d70a454e6f66e54ebae8014a4f783518b17654d4
DenmarkPolice <albin.persson@protonmail.com>
2022-06-18 16:24:26 +0200

    Linter errors

--- src/syntax/syntax_highlight.cpp
+++ src/syntax/syntax_highlight.cpp
@@ -1,10 +1,11 @@
+// Copyright 2022 Albin Persson
 #include "include/syntax_highlight.hpp"
 
-const char* SYNTAX_YELLOW = "\x1b[33m";
-const char* SYNTAX_BLUE = "\x1b[36m";
-const char* SYNTAX_GREEN = "\x1b[92m";
-const char* SYNTAX_BRIGHT_YELLOW = "\x1b[93m";
-const char* SYNTAX_RESET = "\x1b[0m";
+// const char* S_YELLOW = "\x1b[33m";
+const char* S_BLUE = "\x1b[36m";
+const char* S_GREEN = "\x1b[92m";
+const char* S_YELLOW = "\x1b[93m";
+const char* S_RESET = "\x1b[0m";
 int SIZE_OF_COLOR_ESC = 5;
 int SIZE_OF_RESET_ESC = 4;
 
@@ -21,7 +22,7 @@ int SyntaxHighlight::detectFiletype(char* file_ext) {
 std::string SyntaxHighlight::hightlightLine(char* line, char* file_ext) {
     int c = detectFiletype(file_ext);
 
-    switch(c) {
+    switch (c) {
         // C/C++ code
         case 1:
             return cppStyle(line);
@@ -35,8 +36,7 @@ std::string SyntaxHighlight::hightlightLine(char* line, char* file_ext) {
 }
 
 std::string SyntaxHighlight::cppStyle(char* line) {
-
-    //Regex combinations
+    // Regex combinations
     auto const regex_string = std::regex("\".*\"");
     auto const regex_comment = std::regex("(//).*");
     auto const regex_statement = std::regex(
@@ -54,28 +54,30 @@ std::string SyntaxHighlight::cppStyle(char* line) {
         ++it) {
             added_length += (SIZE_OF_RESET_ESC + SIZE_OF_COLOR_ESC);
             comment_index = it->position();
-            search_text.insert(it->position()+it->length(), SYNTAX_RESET);
-            search_text.insert(it->position(), SYNTAX_GREEN);
+            search_text.insert(it->position()+it->length(), S_RESET);
+            search_text.insert(it->position(), S_GREEN);
             break;
         }
     }
 
     auto is_string = std::regex_search(search_text, search_results, regex_string);
     if (is_string) {
-        std::vector<std::pair<int,int>> index_length;
+        std::vector<std::pair<int, int>> index_length;
         for (auto it = std::sregex_iterator(search_text.begin(), search_text.end(), regex_string);
         it != std::sregex_iterator();
         ++it) {
             if (comment_index != -1 && comment_index < it->position()) {
                 break;
             }
-            if(it->length() > 0) {
+            if (it->length() > 0) {
                 index_length.push_back(std::make_pair<int, int>(it->position(), it->length()));
             }
         }
         for (int found_index = index_length.size(); found_index > 0; found_index--) {
-            search_text.insert(index_length[found_index-1].first+index_length[found_index-1].second, SYNTAX_RESET);
-            search_text.insert(index_length[found_index-1].first, SYNTAX_BRIGHT_YELLOW);
+            std::pair<int, int> current_index = index_length[found_index-1];
+            search_text.insert(
+                current_index.first+current_index.second, S_RESET);
+            search_text.insert(current_index.first, S_YELLOW);
             added_length += (SIZE_OF_RESET_ESC + SIZE_OF_COLOR_ESC);
         }
     }
@@ -89,13 +91,15 @@ std::string SyntaxHighlight::cppStyle(char* line) {
             if (comment_index != -1 && comment_index < it->position()) {
                 break;
             }
-            if(it->length() > 0) {
+            if (it->length() > 0) {
                 index_length.push_back(std::make_pair<int, int>(it->position(), it->length()));
             }
         }
         for (int found_index = index_length.size(); found_index > 0; found_index--) {
-            search_text.insert(index_length[found_index-1].first+index_length[found_index-1].second, SYNTAX_RESET);
-            search_text.insert(index_length[found_index-1].first, SYNTAX_BRIGHT_YELLOW);
+            std::pair<int, int> current_index = index_length[found_index-1];
+            search_text.insert(
+                current_index.first+current_index.second, S_RESET);
+            search_text.insert(current_index.first, S_YELLOW);
             added_length += (SIZE_OF_RESET_ESC + SIZE_OF_COLOR_ESC);
         }
     }
@@ -109,13 +113,15 @@ std::string SyntaxHighlight::cppStyle(char* line) {
             if (comment_index != -1 && comment_index < it->position()) {
                 break;
             }
-            if(it->length() > 0) {
+            if (it->length() > 0) {
                 index_length.push_back(std::make_pair<int, int>(it->position(), it->length()));
             }
         }
         for (int found_index = index_length.size(); found_index > 0; found_index--) {
-            search_text.insert(index_length[found_index-1].first+index_length[found_index-1].second, SYNTAX_RESET);
-            search_text.insert(index_length[found_index-1].first, SYNTAX_BRIGHT_YELLOW);
+            std::pair<int, int> current_index = index_length[found_index-1];
+            search_text.insert(
+                current_index.first+current_index.second, S_RESET);
+            search_text.insert(current_index.first, S_YELLOW);
             added_length += (SIZE_OF_RESET_ESC + SIZE_OF_COLOR_ESC);
         }
     }
