f279ec2da6fbbe4f3e7ec58fceb34fa3ffcf7394
Andreas Bauer <andreas-bauer@users.noreply.github.com>
2022-07-19 18:26:07 +0200

    Update file_type.cpp

--- src/file_type/file_type.cpp
+++ src/file_type/file_type.cpp
@@ -38,25 +38,4 @@ namespace filetype {
         return FileType::txt;
     }
 
-    TEST_CASE("should return correct FileType") {
-        CHECK(fileTypeFromStr("") == FileType::txt);
-        CHECK(fileTypeFromStr("UNKNOWN") == FileType::txt);
-
-        CHECK(fileTypeFromStr("c") == FileType::cpp);
-        CHECK(fileTypeFromStr("cc") == FileType::cpp);
-        CHECK(fileTypeFromStr("cpp") == FileType::cpp);
-        CHECK(fileTypeFromStr("h") == FileType::cpp);
-        CHECK(fileTypeFromStr("hpp") == FileType::cpp);
-
-        CHECK(fileTypeFromStr("md") == FileType::md);
-        CHECK(fileTypeFromStr("mkdn") == FileType::md);
-        CHECK(fileTypeFromStr("mdown") == FileType::md);
-        CHECK(fileTypeFromStr("markdown") == FileType::md);
-        CHECK(fileTypeFromStr("mkd") == FileType::md);
-        CHECK(fileTypeFromStr("rmd") == FileType::md);
-
-        CHECK(fileTypeFromStr("CPP") == FileType::cpp);
-        CHECK(fileTypeFromStr("cPp") == FileType::cpp);
-    }
-
 } // namespace filetype
