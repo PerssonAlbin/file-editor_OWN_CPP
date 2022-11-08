8fb8187105bd81e115e1c01e28ff22b7a8219f01
DenmarkPolice <albin.persson@protonmail.com>
2022-11-08 18:42:01 +0100

    Test is fully automatic

--- tests/file_editor/buffer_handling_test.cpp
+++ tests/file_editor/buffer_handling_test.cpp
@@ -1,8 +1,20 @@
 #include "doctest/doctest.h"
 #include "include/file_editor.hpp"
+#include <fstream>
 
-TEST_CASE("test buffer handling") {
+TEST_CASE("Loading of files") {
     std::string test_path = "../test_env/";
+    std::string comparison = "Fivel\n\nother info\n";
+
+    if (!fs::is_directory(test_path) || !fs::exists(test_path)) {
+        fs::create_directory(test_path);
+    }
+
+    std::ofstream test_file;
+    test_file.open(test_path + "file.txt");
+    test_file << comparison;
+    test_file.close();
+
     FileEditor test(test_path);
     test.runtime(false);
 
@@ -10,6 +22,6 @@ TEST_CASE("test buffer handling") {
     const char* output = test.editorRowToString(&len);
     std::string output_str(output);
 
-    std::string comparison = "Fivel\n";
     CHECK(output_str == comparison);
+    CHECK(output_str.size() == comparison.size());
 }
